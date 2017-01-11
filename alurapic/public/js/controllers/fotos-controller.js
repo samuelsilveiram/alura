7c7817b8fea2793cb439c13b0176d0117f336b7b
Samuel Manoel Silveira <samuel.involves@gmail.com>
2017-01-11 00:27:56 -0200

    Adicionando a pasta alupapic no commit

---
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -0,0 +1,8 @@
+angular.module('alurapic').controller('FotosController', function($scope) {
+
+   $scope.foto = {
+       titulo : "Leão",
+       url : "http://www.ceudasemana.com.br/wp-content/uploads/2012/08/leao-em-fogo1-300x300.jpg"
+   }
+
+});
