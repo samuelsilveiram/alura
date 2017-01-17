8e090451e9d4630c6a6790f783a43110f279e21a
Samuel Manoel Silveira <samuel.involves@gmail.com>
2017-01-17 00:08:53 -0200

    Evolução do treinamento até a aula 6, segundo commit

--- alurapic/public/js/directives/minhas-diretivas.js
+++ alurapic/public/js/directives/minhas-diretivas.js
@@ -1,17 +1,20 @@
 angular.module('minhasDiretivas', [])
 .directive('meuPainel', function(){
     var ddo = {}; //DDO Directive Definition Object
-
     ddo.restric = "AE" //Atribute Element
-
     ddo.scope = {
         titulo: '@titulo' // Pode ser colocado somente o @
     };
-
     ddo.transclude = true;
-
     ddo.templateUrl = 'js/directives/meu-painel.html';
-
-
+    return ddo;
+}).directive('minhaFoto', function(){
+    var ddo = {};
+    ddo.restric = 'AE'
+    ddo.scope = {
+        url: '@',
+        titulo: '@'
+    };
+    ddo.templateUrl = 'js/directives/minha-foto.html';
     return ddo;
 });
