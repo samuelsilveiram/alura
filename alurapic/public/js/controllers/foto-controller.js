8dae23b13ea29ef856e49d8cbd98d523a47a6105
Samuel Manoel Silveira <samuel.involves@gmail.com>
2017-01-23 22:52:02 -0200

    final da setima aula

--- alurapic/public/js/controllers/foto-controller.js
+++ alurapic/public/js/controllers/foto-controller.js
@@ -1,8 +1,18 @@
-angular.module('alurapic').controller('FotoController', function($scope){
+angular.module('alurapic').controller('FotoController', function($scope, $http){
     
     $scope.foto = {};
+    $scope.mensagem = '';
 
     $scope.submeter = function(){
-        console.log($scope.foto);
+        if($scope.formulario.$valid){
+            $http.post('v1/fotos', $scope.foto)
+            .success(function(){
+                $scope.foto = {};
+                $scope.mensagem = 'Foto cadastrada com sucesso';
+            })
+            .error(function(){
+                $scope.mensagem = 'Ocorreu um erro ao salvar a foto';
+            });
+        }
     };
 });
